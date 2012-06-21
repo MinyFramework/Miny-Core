7ec429d6810bd5f5e9334cf507bb1f6a72e61a9a
bugadani <bugadani@bugadani-inspiron>
2012-06-21 11:46:38 +0200

    Renamed AutoLoader and removed unnecessary bootstrap file.

--- bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include FW_DIR . '/Autoloader.php';
-\Miny\AutoLoader::register('\\Miny', FW_DIR);
-\Miny\AutoLoader::init();
-
-//TODO: kód takarítás - 1: metódusokat szétszedni getX/setX-re
