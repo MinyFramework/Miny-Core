76665eca02260be358a0da41a427e6eab0de7587
bugadani <bugadani@bugadani-inspiron>
2012-07-01 16:13:07 +0200

    Response: cleanup and new method: remove HTTP header from response.

--- HTTP/Response.php
+++ HTTP/Response.php
@@ -28,6 +28,48 @@ namespace Miny\HTTP;
 
 class Response
 {
+    public static $status_codes = array(
+        100      => 'Continue',
+        101      => 'Switching Protocols',
+        200      => 'OK',
+        201      => 'Created',
+        202      => 'Accepted',
+        203      => 'Non-Authoritative Information',
+        204      => 'No Content',
+        205      => 'Reset Content',
+        206      => 'Partial Content',
+        300      => 'Multiple Choices',
+        301      => 'Moved Permanently',
+        302      => 'Found',
+        303      => 'See Other',
+        304      => 'Not Modified',
+        305      => 'Use Proxy',
+        306      => 'Temporary Redirect',
+        400      => 'Bad Request',
+        401      => 'Unauthorized',
+        402      => 'Payment Required',
+        403      => 'Forbidden',
+        404      => 'Not Found',
+        405      => 'Method Not Allowed',
+        406      => 'Not Acceptable',
+        407      => 'Proxy Authentication Required',
+        408      => 'Request Timeout',
+        409      => 'Conflict',
+        410      => 'Gone',
+        411      => 'Length Required',
+        412      => 'Precondition Failed',
+        413      => 'Request Entity Too Large',
+        414      => 'Request-URI Too Long',
+        415      => 'Unsupported Media Type',
+        416      => 'Requested Range Not Satisfiable',
+        417      => 'Expectation Failed',
+        500      => 'Internal Server Error',
+        501      => 'Not Implemented',
+        502      => 'Bad Gateway',
+        503      => 'Service Unavailable',
+        504      => 'Gateway Timeout',
+        505      => 'HTTP Version Not Supported'
+    );
     private $content;
     private $cookies = array();
     private $headers = array();
@@ -66,6 +108,24 @@ class Response
         }
     }
 
+    public function removeHeader($name, $value = NULL)
+    {
+        if (is_null($value)) {
+            unset($this->headers[$name]);
+        } else {
+            if (!isset($this->headers[$name])) {
+                return;
+            }
+            if (!is_array($this->headers[$name])) {
+                return;
+            }
+            if(!is_array($value)) {
+                $value = array($value);
+            }
+            $this->headers[$name] = array_diff($this->headers[$name], $value);
+        }
+    }
+
     public function getHeaders()
     {
         return $this->headers;
@@ -86,61 +146,15 @@ class Response
         return $this->content;
     }
 
-    public function __toString()
-    {
-        return $this->content;
-    }
-
     private function sendHTTPStatus()
     {
-        if ($this->status_code !== NULL) {
-            $codes = array(
-                100 => 'Continue',
-                101 => 'Switching Protocols',
-                200 => 'OK',
-                201 => 'Created',
-                202 => 'Accepted',
-                203 => 'Non-Authoritative Information',
-                204 => 'No Content',
-                205 => 'Reset Content',
-                206 => 'Partial Content',
-                300 => 'Multiple Choices',
-                301 => 'Moved Permanently',
-                302 => 'Found',
-                303 => 'See Other',
-                304 => 'Not Modified',
-                305 => 'Use Proxy',
-                306 => 'Temporary Redirect',
-                400 => 'Bad Request',
-                401 => 'Unauthorized',
-                402 => 'Payment Required',
-                403 => 'Forbidden',
-                404 => 'Not Found',
-                405 => 'Method Not Allowed',
-                406 => 'Not Acceptable',
-                407 => 'Proxy Authentication Required',
-                408 => 'Request Timeout',
-                409 => 'Conflict',
-                410 => 'Gone',
-                411 => 'Length Required',
-                412 => 'Precondition Failed',
-                413 => 'Request Entity Too Large',
-                414 => 'Request-URI Too Long',
-                415 => 'Unsupported Media Type',
-                416 => 'Requested Range Not Satisfiable',
-                417 => 'Expectation Failed',
-                500 => 'Internal Server Error',
-                501 => 'Not Implemented',
-                502 => 'Bad Gateway',
-                503 => 'Service Unavailable',
-                504 => 'Gateway Timeout',
-                505 => 'HTTP Version Not Supported'
-            );
-            if (isset($codes[$this->status_code])) {
-                $code = $codes[$this->status_code];
-                $header = sprintf('HTTP/1.1 %d: %s', $this->status_code, $code);
-                header($header, true, $this->status_code);
-            }
+        if (is_null($this->status_code)) {
+            return;
+        }
+        if (isset(self::$status_codes[$this->status_code])) {
+            $code = self::$status_codes[$this->status_code];
+            $header = sprintf('HTTP/1.1 %d: %s', $this->status_code, $code);
+            header($header, true, $this->status_code);
         }
     }
 
