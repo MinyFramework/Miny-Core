d1660114f114de27278cc40595e6d3ad77b6fdcd
bugadani <bugadani@bugadani-inspiron>
2012-07-05 18:49:55 +0200

    Entity package

---
+++ Entity/Entity.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version accepted by the author in accordance with section
+ * 14 of the GNU General Public License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   Miny/Entity
+ * @copyright 2012 Dániel Buga <daniel@bugadani.hu>
+ * @license   http://www.gnu.org/licenses/gpl.txt
+ *            GNU General Public License
+ * @version   1.0
+ */
+
+namespace Miny\Entity;
+
+abstract class Entity implements \ArrayAccess
+{
+    public abstract function getKeyName();
+    public function getKey()
+    {
+        $key = $this->getKeyName();
+        return $this->$key;
+    }
+
+    public function checkField($name)
+    {
+        if (!property_exists($this, $name)) {
+            throw new \InvalidArgumentException('Field not exists: ' . $name);
+        }
+    }
+
+    public function __isset($name)
+    {
+        return property_exists($this, $name);
+    }
+
+    public function __set($field, $value)
+    {
+        $this->checkField($field);
+        $setter = 'set' . ucfirst(strtolower($field));
+        if (method_exists($this, $setter)) {
+            $this->$setter($value);
+        } else {
+            $this->$field = $value;
+        }
+    }
+
+    public function __get($field)
+    {
+        $this->checkField($field);
+        $getter = 'get' . ucfirst(strtolower($field));
+        return method_exists($this, $getter) ? $this->$getter() : $this->$field;
+    }
+
+    public function toArray()
+    {
+        return get_object_vars($this);
+    }
+
+    public function offsetExists($offset)
+    {
+        return $this->__isset($offset);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->__get($offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        return $this->__set($offset, $value);
+    }
+
+    public function offsetUnset($offset)
+    {
+        return $this->__set($offset, NULL);
+    }
+
+}
---
+++ Entity/EntityProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version accepted by the author in accordance with section
+ * 14 of the GNU General Public License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   Miny/Entity
+ * @copyright 2012 Dániel Buga <daniel@bugadani.hu>
+ * @license   http://www.gnu.org/licenses/gpl.txt
+ *            GNU General Public License
+ * @version   1.0
+ */
+
+namespace Miny\Entity;
+
+abstract class EntityProvider
+{
+    private $entity_class;
+    private $entities = array();
+
+    public function __construct($entity_class)
+    {
+        $this->entity_class = $entity_class;
+    }
+
+    public function add(Entity $ent)
+    {
+        $this->entities[$ent->getKey()] = $ent;
+    }
+
+    public function get($key)
+    {
+        if (!$this->has($key)) {
+            throw new \OutOfBoundsException('Entity not set: ' . $key);
+        }
+        return $this->entities[$key];
+    }
+
+    public function remove($key)
+    {
+        if (isset($this->entities[$key])) {
+            unset($this->entities[$key]);
+            return true;
+        }
+    }
+
+    public function has($key)
+    {
+        return isset($this->entities[$key]);
+    }
+
+}
