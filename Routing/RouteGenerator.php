64de7c281842cea008d4d6d6b26da2d15091c753
bugadani <bugadani@bugadani-inspiron>
2012-07-13 15:11:53 +0200

    [Routing] Initial commit of routing rework.

---
+++ Routing/RouteGenerator.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version accepted by the author in accordance with section
+ * 14 of the GNU General Public License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   Miny/Routing
+ * @copyright 2012 Dániel Buga <daniel@bugadani.hu>
+ * @license   http://www.gnu.org/licenses/gpl.txt
+ *            GNU General Public License
+ * @version   1.0
+ */
+
+namespace Miny\Routing;
+
+class RouteGenerator
+{
+    private $routes;
+
+    public function __construct(RouteCollection $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function generate($route_name, array $parameters = array())
+    {
+        foreach ($this->routes as $name => $route) {
+            if ($route_name !== $name) {
+                continue;
+            }
+
+            $required_params = $route->getParameterNames();
+            $missing = array_diff($required_params, $parameters);
+            if (!empty($missing)) {
+                $message = 'Parameters not set: ' . join(', ', $missing);
+                throw new \InvalidArgumentException($message);
+            }
+
+            $path = $route->getPath();
+            $first_http_param = strpos($path, '?') === false;
+            foreach ($parameters as $name => $value) {
+                if (strpos($path, ':' . $name) !== false) {
+                    $path = str_replace(':' . $name, $value, $path);
+                } elseif ($first_http_param) {
+                    $path .= sprintf('?%s=%s', $name, $value);
+                } else {
+                    $path .= sprintf('&%s=%s', $name, $value);
+                }
+            }
+            return $path;
+        }
+    }
+
+}
---
+++ Routing/RouteMatcher.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version accepted by the author in accordance with section
+ * 14 of the GNU General Public License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   Miny/Routing
+ * @copyright 2012 Dániel Buga <daniel@bugadani.hu>
+ * @license   http://www.gnu.org/licenses/gpl.txt
+ *            GNU General Public License
+ * @version   1.0
+ */
+
+namespace Miny\Routing;
+
+class RouteMatcher
+{
+    private $routes;
+
+    public function __construct(RouteCollection $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function match($path, $method = NULL)
+    {
+        foreach ($this->routes as $route) {
+            $route_method = $route->getMethod();
+            if ($method !== NULL && $route_method !== NULL) {
+                if ($method !== $route_method) {
+                    continue;
+                }
+            }
+
+            if ($route->isStatic()) {
+                if ($path != $route->getPath()) {
+                    continue;
+                }
+                return new Match($route);
+            }
+
+            $matched = array();
+            if (preg_match('#^' . $route->getRegex() . '$#Du', $path, $matched)) {
+                $matched_params = array();
+                $parameter_count = $route->getParameterCount();
+                for ($i = 1; $i < $parameter_count; ++$i) {
+                    $matched_params[$route->getParameterName($i - 1)] = $v;
+                }
+                return new Match($route, $matched_params);
+            }
+        }
+        return false;
+    }
+
+}
