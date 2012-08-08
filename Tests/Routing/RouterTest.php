19d9b5082e479a4e04c0abd1596cd2f41d44dc69
bugadani <bugadani@bugadani-inspiron>
2012-08-08 20:03:57 +0200

    Add RouteTest

---
+++ Tests/Routing/RouterTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Miny\Routing;
+
+require_once dirname(__FILE__) . '/../../Routing/RouteCollection.php';
+require_once dirname(__FILE__) . '/../../Routing/Router.php';
+
+class RouterTest extends \PHPUnit_Framework_TestCase
+{
+    protected $object;
+
+    protected function setUp()
+    {
+        $this->object = new Router;
+    }
+
+    public function testSimpleRoutes()
+    {
+
+    }
+
+}
+
+?>
