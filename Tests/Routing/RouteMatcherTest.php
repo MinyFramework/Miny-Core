077f15a0027baa92dd491842dc12a2ed8613634c
bugadani <bugadani@bugadani-inspiron>
2012-08-08 21:06:41 +0200

    Add cases to RouteMatcherTest

--- Tests/Routing/RouteMatcherTest.php
+++ Tests/Routing/RouteMatcherTest.php
@@ -10,14 +10,20 @@ require_once dirname(__FILE__) . '/../../Routing/RouteMatcher.php';
 class RouteMatcherTest extends \PHPUnit_Framework_TestCase
 {
     protected $route;
+    protected $static_route;
+    protected $static_route_post;
     protected $object;
 
     protected function setUp()
     {
         $collection = new RouteCollection;
+        $this->static_route = new Route('static_path');
+        $this->static_route_post = new Route('other_static_path', 'POST');
         $this->route = new Route('path/:parameter', 'POST');
         $this->route->specify('parameter', '(\d+)');
         $collection->addRoute($this->route);
+        $collection->addRoute($this->static_route);
+        $collection->addRoute($this->static_route_post);
         $this->object = new RouteMatcher($collection);
     }
 
@@ -29,11 +35,25 @@ class RouteMatcherTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($this->object->match('path/5', 'PUT'));
         //wrong pattern
         $this->assertFalse($this->object->match('path/foo'));
-        //match
-        $match = $this->object->match('path/5');
+        //match NULL method
+        $this->assertInstanceOf(__NAMESPACE__ . '\Match', $this->object->match('path/5'));
+        //match specific method
+        $match = $this->object->match('path/5', 'POST');
         $this->assertInstanceOf(__NAMESPACE__ . '\Match', $match);
+        //check if match has the expected route
         $this->assertSame($this->route, $match->getRoute());
         $this->assertEquals(array('parameter' => 5), $match->getParameters());
+    }
+
+    public function testMatchStaticRoute()
+    {
+        //should match any method
+        $this->assertInstanceOf(__NAMESPACE__ . '\Match', $this->object->match('static_path'));
+        $this->assertInstanceOf(__NAMESPACE__ . '\Match', $this->object->match('static_path', 'any_method'));
+        //should only match POST (or NULL)
+        $this->assertFalse($this->object->match('other_static_path', 'any_method_except_post'));
+        $this->assertInstanceOf(__NAMESPACE__ . '\Match', $this->object->match('other_static_path'));
+        $this->assertInstanceOf(__NAMESPACE__ . '\Match', $this->object->match('other_static_path', 'POST'));
     }
 
 }
