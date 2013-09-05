5bc091d8f68cf7ca23ce507feb70a373407315e6
Buga Dániel <daniel@bugadani.hu>
2013-09-05 23:25:49 +0200

    Preliminary work on new templating subsystem - work in progress

---
+++ View/Exceptions/ViewNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
+ *
+ * For licensing information see the LICENSE file.
+ */
+
+namespace Miny\View\Exceptions;
+
+use RuntimeException;
+
+class ViewNotFoundException extends RuntimeException
+{
+
+}
+
