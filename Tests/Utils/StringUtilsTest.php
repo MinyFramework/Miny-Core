80a3dded6da8fce230896a678d12c29be8274e5a
Buga Dániel <daniel@bugadani.hu>
2014-01-08 21:12:43 +0100

    Test case for StringUtils

---
+++ Tests/Utils/StringUtilsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Miny\Utils;
+
+use PHPUnit_Framework_TestCase;
+
+require_once dirname(__FILE__) . '/../../Utils/StringUtils.php';
+
+class StringUtilsTest extends PHPUnit_Framework_TestCase
+{
+    public function testCompare()
+    {
+        $this->assertFalse(StringUtils::compare('string', 'ing'));
+        $this->assertFalse(StringUtils::compare('length_1', 'length_2'));
+        $this->assertTrue(StringUtils::compare('string', 'string'));
+    }
+
+    public function testStartsWith()
+    {
+        $this->assertTrue(StringUtils::startsWith('string', 'str'));
+        $this->assertFalse(StringUtils::startsWith('foo', 'bar'));
+    }
+
+    public function testEndsWith()
+    {
+        $this->assertTrue(StringUtils::endsWith('string', 'ing'));
+        $this->assertFalse(StringUtils::endsWith('foo', 'bar'));
+    }
+}
+
+?>
