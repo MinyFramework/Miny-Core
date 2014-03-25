dac31ba56f3e865130f0fc47dfb6cf5c3087f32a
Dániel Buga <bugadani@gmail.com>
2014-03-26 00:08:47 +0100

    More tests for Log.

--- Tests/Log/LogTest.php
+++ Tests/Log/LogTest.php
@@ -143,4 +143,49 @@ class LogTest extends \PHPUnit_Framework_TestCase
 
         $this->log->setFlushLimit(5);
     }
+
+    public function testThatGetLevelNameReturnsUnknown()
+    {
+        $this->assertStringStartsNotWith('Unknown', Log::getLevelName(Log::PROFILE));
+        $this->assertStringStartsNotWith('Unknown', Log::getLevelName(Log::INFO));
+        $this->assertStringStartsNotWith('Unknown', Log::getLevelName(Log::DEBUG));
+        $this->assertStringStartsNotWith('Unknown', Log::getLevelName(Log::WARNING));
+        $this->assertStringStartsNotWith('Unknown', Log::getLevelName(Log::ERROR));
+        $this->assertStringStartsWith('Unknown', Log::getLevelName(150));
+    }
+
+    public function testThatMessagesAreFormatted()
+    {
+        $this->writerMock->expects($this->at(0))
+            ->method('add')
+            ->with(
+                $this->callback(
+                    function (LogMessage $message) {
+                        return $message->getMessage() == 'message formatted with arguments';
+                    }
+
+                )
+            );
+        $this->writerMock->expects($this->at(1))
+            ->method('add')
+            ->with(
+                $this->callback(
+                    function (LogMessage $message) {
+                        $string = 'message formatted with arguments from array';
+
+                        return $message->getMessage() == $string;
+                    }
+                )
+            );
+        $this->log->registerWriter($this->writerMock, Log::INFO);
+
+        $this->log->write(Log::INFO, 'category', 'message %s %s', 'formatted', 'with arguments');
+        $this->log->write(
+            Log::INFO,
+            'category',
+            'message %s %s',
+            array('formatted', 'with arguments from array')
+        );
+        $this->log->flush();
+    }
 }
