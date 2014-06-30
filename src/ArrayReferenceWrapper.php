670999489414f5e5d76608006538d4a903c6c505
Buga Dániel <bugadani@gmail.com>
2014-06-30 20:51:50 +0200

    New: ArrayReferenceWrapper that can be used to simulate pass-by-reference-like behaviour.

---
+++ src/ArrayReferenceWrapper.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ * (c) Dániel Buga <bugadani@gmail.com>
+ *
+ * For licensing information see the LICENSE file.
+ */
+
+namespace Miny;
+
+/**
+ * Class ArrayReferenceWrapper acts as a thin wrapper around arrays to simulate pass-by-reference
+ * behaviour.
+ *
+ * @author Dániel Buga <bugadani@gmail.com>
+ * @package Miny
+ */
+class ArrayReferenceWrapper implements \ArrayAccess
+{
+    private $data;
+
+    public function __construct(array &$data)
+    {
+        $this->data = & $data;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->data);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function &offsetGet($offset)
+    {
+        return $this->data[$offset];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
+    }
+}
---
+++ test/ArrayReferenceWrapperTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Miny;
+
+class ArrayReferenceWrapperTest extends \PHPUnit_Framework_TestCase
+{
+    private $array;
+    private $object;
+
+    public function setUp()
+    {
+        $this->array  = array('foo', 'bar', new \stdClass(), array(1, 2));
+        $this->object = new ArrayReferenceWrapper($this->array);
+    }
+
+    public function testValuesAreReturnedByReference()
+    {
+        $this->assertSame($this->array[2], $this->object[2]);
+    }
+
+    public function testValuesAreChangedInTheSourceArray()
+    {
+        unset($this->object[2]);
+        $this->object[1]    = 'foobar';
+        $this->object[3][2] = 3;
+        $this->object[4]    = 'baz';
+
+        $this->assertEquals('foobar', $this->array[1]);
+        $this->assertEquals('baz', $this->array[4]);
+        $this->assertEquals(3, $this->array[3][2]);
+        $this->assertFalse(isset($this->array[2]));
+    }
+}
