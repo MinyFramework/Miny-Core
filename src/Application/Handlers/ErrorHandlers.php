6ec39489528f21c24735af8f17f7436b80700b1a
Buga Dániel <bugadani@gmail.com>
2014-07-13 18:15:24 +0200

    Convert all catchable error types to exceptions. Source: http://php.net/manual/en/function.set-error-handler.php#112881

--- src/Application/Handlers/ErrorHandlers.php
+++ src/Application/Handlers/ErrorHandlers.php
@@ -15,15 +15,6 @@ use Miny\Log\Log;
 
 class ErrorHandlers
 {
-    private static $internalLogCategories = array(
-        E_NOTICE       => 'Notice (PHP)',
-        E_USER_NOTICE  => 'Notice',
-        E_WARNING      => 'Warning (PHP)',
-        E_USER_WARNING => 'Warning',
-        E_DEPRECATED   => 'Deprecated notice (PHP)',
-        E_STRICT       => 'Strict notice (PHP)'
-    );
-
     /**
      * @var Log
      */
@@ -58,18 +49,103 @@ class ErrorHandlers
         }
     }
 
-    public function handleErrors($errNo, $errStr, $errFile, $errLine)
+    /**
+     * @see http://php.net/manual/en/function.set-error-handler.php#112881
+     */
+    public function handleErrors($errType, $errMsg, $errFile, $errLine)
     {
-        if (!isset(self::$internalLogCategories[$errNo])) {
-            throw new \ErrorException($errStr, $errNo, 0, $errFile, $errLine);
+        if (error_reporting() & $errType === 0) {
+            // This error code is not included in error_reporting
+            return;
         }
-        $this->log->write(
-            Log::WARNING,
-            self::$internalLogCategories[$errNo],
-            '%s in %s on line %s',
-            $errStr,
-            $errFile,
-            $errLine
-        );
+        switch ($errType) {
+            default:
+            case E_ERROR:
+                throw new \ErrorException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_WARNING:
+                throw new WarningException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_PARSE:
+                throw new ParseException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_NOTICE:
+                throw new NoticeException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_CORE_ERROR:
+                throw new CoreErrorException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_CORE_WARNING:
+                throw new CoreWarningException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_COMPILE_ERROR:
+                throw new CompileErrorException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_COMPILE_WARNING:
+                throw new CoreWarningException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_USER_ERROR:
+                throw new UserErrorException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_USER_WARNING:
+                throw new UserWarningException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_USER_NOTICE:
+                throw new UserNoticeException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_STRICT:
+                throw new StrictException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_RECOVERABLE_ERROR:
+                throw new RecoverableErrorException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_DEPRECATED:
+                throw new DeprecatedException($errMsg, 0, $errType, $errFile, $errLine);
+            case E_USER_DEPRECATED:
+                throw new UserDeprecatedException($errMsg, 0, $errType, $errFile, $errLine);
+        }
     }
+}
+
+class WarningException extends \ErrorException
+{
+}
+
+class ParseException extends \ErrorException
+{
+}
+
+class NoticeException extends \ErrorException
+{
+}
+
+class CoreErrorException extends \ErrorException
+{
+}
+
+class CoreWarningException extends \ErrorException
+{
+}
+
+class CompileErrorException extends \ErrorException
+{
+}
+
+class CompileWarningException extends \ErrorException
+{
+}
+
+class UserErrorException extends \ErrorException
+{
+}
+
+class UserWarningException extends \ErrorException
+{
+}
+
+class UserNoticeException extends \ErrorException
+{
+}
+
+class StrictException extends \ErrorException
+{
+}
+
+class RecoverableErrorException extends \ErrorException
+{
+}
+
+class DeprecatedException extends \ErrorException
+{
+}
+
+class UserDeprecatedException extends \ErrorException
+{
 }
